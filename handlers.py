e79b9add81f427fb6fb4a73b8f9b1d9f5112cc4e
musayevjahongir <musayevjahongir5051@gmail.com>
2023-12-22 09:22:55 +0500

    weather

--- handlers.py
+++ handlers.py
@@ -7,7 +7,9 @@ import requests
 def start(update: Update, context: CallbackContext):
     user = update.effective_user
     bot = context.bot
-    bot.send_message(user.id, 'Asslomu alaykum.', reply_markup=start_keyboard)
+    text=f"""Assalomu alaykum {user.first_name},
+O'zingiz turgan joydagi ob havoni bilish uchun bosing👇"""
+    bot.send_message(user.id, text, reply_markup=start_keyboard)
 
 def send_location(update: Update, context: CallbackContext):
     user = update.effective_user
@@ -17,9 +19,11 @@ def send_location(update: Update, context: CallbackContext):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
     response = requests.get(url).json()
 
-    weather = f"""
+    weather = f"""Your location {response["name"]}
 ☀️weather-{response['weather'][0]['main']},
-🌡temp-{response["main"]['temp']-273.15}
+🌡temp-{round (response["main"]['temp']-273.15, 2)}
+Namlik-{response["main"]['humidity']}%
+Shamol tezligi-{response["wind"]['speed']}
 """
 
     bot.sendMessage(user.id,weather , reply_markup=start_keyboard)
